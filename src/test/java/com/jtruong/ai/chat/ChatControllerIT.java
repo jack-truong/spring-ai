c1623135660aa04197b1490797a5b35ba8c9dbf4
Jack Truong <jackiextruong@yahoo.com>
2024-05-27 17:01:17 -0700

    Make tests pass

--- src/test/java/com/jtruong/ai/chat/ChatControllerIT.java
+++ src/test/java/com/jtruong/ai/chat/ChatControllerIT.java
@@ -2,6 +2,12 @@ package com.jtruong.ai.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.metadata.ChatResponseMetadata;
+import org.springframework.ai.chat.metadata.EmptyUsage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,29 +16,23 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ChatControllerIT {
-
+public class ChatControllerIT extends BaseChatControllerTest {
   @Autowired
   private TestRestTemplate template;
-
-  @MockBean
-  private ChatClient chatClient;
-
-  @MockBean
-  private ImageClient imageClient;
 
   @Test
   public void getHello() throws Exception {
     // given
-    when(chatClient.call("foo")).thenReturn("bar");
+    setupMockChatResponse("hello", "greetings");
 
     // when
-    ResponseEntity<String> response = template.getForEntity("/ai/chat?prompt=foo", String.class);
+    ResponseEntity<String> response = template.getForEntity("/ai/chat?prompt=hello", String.class);
 
     // then
-    assertThat(response.getBody()).isEqualTo("bar");
+    assertThat(response.getBody()).isEqualTo("greetings");
   }
 }
